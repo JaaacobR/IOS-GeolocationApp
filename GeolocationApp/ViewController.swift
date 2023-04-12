76dcc42ca7d79b3e2f51d8cc19b8d50df37b4908
Jakub Ryszka <ryszkajakub6@gmail.com>
2023-04-12 10:10:11 +0200

    location works

--- GeolocationApp/ViewController.swift
+++ GeolocationApp/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
+
+
+class ViewController: UIViewController, MKMapViewDelegate,CLLocationManagerDelegate  {
     @IBOutlet weak var mapKitView: MKMapView!
+    
+    
+
+    // wywoływana co update lokalizacji
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        locations.first?.coordinate
+        // .coordinate CLLocationCoordinate2D()
+        
+        // tu Adaś
+        let cords = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude:50.06146865220069,
+            longitude: 19.93800239600079), latitudinalMeters: 100, longitudinalMeters: 0) //     max_lat/max_long - potestuj w [m]
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let lm = CLLocationManager()
+        lm.requestAlwaysAuthorization()
+        lm.requestWhenInUseAuthorization()
+        lm.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+        lm.startUpdatingLocation()   // stopUpdatingLocation()
+
+        mapKitView.pointOfInterestFilter = MKPointOfInterestFilter(excluding: [.bank]) // including - widoczne/ukryte warstwy
+        mapKitView.showsUserLocation = true
+         mapKitView.userTrackingMode = .followWithHeading // .follow
     }
 
 
